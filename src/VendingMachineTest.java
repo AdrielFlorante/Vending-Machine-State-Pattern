a588867b57d7573db825e14bce8e12d08820e53b
AdrielFlorante <adriel.florante@neu.edu.ph>
2025-02-23 19:58:42 +0800

    Reworked test cases due to issues again

--- src/VendingMachineTest.java
+++ src/VendingMachineTest.java
@@ -5,17 +5,22 @@ public class VendingMachineTest {
 
         // Test behavior in Idle state
         System.out.println("Test Case: In Idle State");
+        vendingMachine.insertCoin(); // Cannot insert a coin while idle
         vendingMachine.selectItem("Coca Cola"); // Should transition to ItemSelected
         vendingMachine.insertCoin(); // Adds 1 coin to balance and transitions to Dispensing
 
-        vendingMachine.insertCoin(); // Shouldn't be able to insert a coin because Dispensing immediately goes back to Idle
-        vendingMachine.selectItem("Coca Cola"); // May work?
+        // Test methods in ItemSelected state
+        System.out.println("\nTest Case: In ItemSelected State");
+        vendingMachine.insertCoin(); // As it turns out, you may insert more coins but this is unintended behavior... You still need to call "Dispense Item" as if pressing the button
+        vendingMachine.selectItem("Pepsi"); // Won't work, because an item is already selected
+        vendingMachine.dispenseItem(); // Will transition to Dispensing state
         vendingMachine.setOutOfOrder(); // Should transition to OutOfOrder
 
         // Test behavior in OutOfOrder state
         System.out.println("\nTest Case: In OutOfOrder State");
         vendingMachine.selectItem("Pepsi"); // Should not work in OutOfOrder state
         vendingMachine.insertCoin(); // Should not work in OutOfOrder state
+        vendingMachine.dispenseItem(); // Will not work
         vendingMachine.setOutOfOrder(); // No change, it's already out of order
     }
 }
